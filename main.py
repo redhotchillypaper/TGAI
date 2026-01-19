962837d806fc189018d4b884f50fd303a51acce8
redhotchillypaper <y.chaklosh@protonmail.ch>
2026-01-18 18:57:52 -0600

    Added channel interaction, .env and .gitignore files

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 import asyncio
 from telethon import TelegramClient, events
+from dotenv import load_dotenv
+import os
+
+load_dotenv() # loading .env
+
+# TODO:
+# Second section, divide the file into two
+# real functions
+# change text to POSTS
 
 
 
+# .env variables:
+api_id = int(os.getenv('API_ID'))
+api_hash = os.getenv('API_HASH')
+bot_token = os.getenv('BOT_TOKEN')
+link_to_channel = os.getenv('CHANNEL_LINK')
+
+# session declaration 
 bot = TelegramClient('bot_session', api_id, api_hash)
 
+
+async def main():
+    await bot.start(bot_token=bot_token)
+    channel = await bot.get_entity(link_to_channel)
+    print('Bot is running. Connected:', bot.is_connected())
+    await bot.send_message(channel, "TEST TEXT") #change text to POSTS
+    await bot.run_until_disconnected()
+
+
+asyncio.run(main())
+
+# for testing shi
 @bot.on(events.NewMessage(pattern='/start'))
 async def start_handler(event):
     await event.reply('hello')
 
-async def main():
-    await bot.start(bot_token=bot_token)
-    print('Bot is running. Connected:', bot.is_connected())
-    await bot.run_until_disconnected()
 
-asyncio.run(main())
+#  (public) or 'https://t.me/+hash' for private. Join if required: from telethon.tl.functions.channels import JoinChannelRequest; await client(JoinChannelRequest(channel)).
+
+# ​
+# Posting Messages
+
+# Require post permissions: await client.send_message(channel, 'Hello from bot!'). Supports text, media: await client.send_file(channel, photo, caption='Post'). Use silent=True to avoid notifications
